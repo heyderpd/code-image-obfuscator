855d1789e9de6faaa33ddb7704cf5d631ad05a92
heyderpd <heyderpd@gmail.com>
2021-11-18 18:42:22 -0300

    convert config to ts

--- src/config.js
+++
@@ -1,8 +0,0 @@
-
-export const colorLength = 3
-
-export const characterLimit = 3
-
-export const wordLength = 8
-
-export const pixelSize = 8
---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+
+export const colorLength: number = 3
+
+export const characterLimit: number = 3
+
+export const wordLength: number = 8
+
+export const pixelSize: number = 8
