a24cc104871da4f616b52f72661618fc602e26b1
heyderpd <heyderpd@gmail.com>
2021-11-19 01:36:53 -0300

    create loadIterator for message

--- src/iterators/message.ts
+++ src/iterators/message.ts
@@ -1,5 +1,6 @@
 import { chunkSize } from '../config'
-import { convertCharToBinary } from '../utils'
+import { convertCharToBinary, convertBinaryToChar } from '../utils'
+import { findMessageHead, findMessageTail } from '../data/patterns'
 
 
 export function convertChunkToData(chunk: string){
@@ -26,6 +27,53 @@ export const LoadIterator = (message: string) => {
           return {
             value: convertChunkToData(message.slice(start, end)),
             done: false,
+          }
+        }
+      }
+    }
+  }
+}
+
+export const loadIterator = (canvas: any) => {
+  return {
+    [Symbol.iterator]: function() {
+      this._initializing = true
+      this._complete = false
+      this._index = 0
+      this._message = ''
+      this._lastChunk = ''
+      this._chunk = ''
+      this._data = ''
+      return {
+        next: (data: string[]) => {
+          this._chunk += data
+            .map(convertBinaryToChar)
+          if (this._chunk.length < chunkSize) {
+            return
+          }
+          this._lastChunk = this._chunk.slice(0, chunkSize)
+          this._chunk = this._chunk.slice(chunkSize)
+          if (this._initializing) {
+            this._initializing = false
+            const start = findMessageHead(this._lastChunk)
+            if (!start) {
+              throw new Error('message not found')
+            }
+            this._lastChunk = this._lastChunk.slice(start.position)
+          }
+          const end = findMessageTail(this._lastChunk, this._chunk)
+          if (end) {
+            const lastChunk = this._lastChunk + this._chunk
+            const length = (end.chunk * chunkSize) + end.position
+            this._lastChunk = lastChunk.slice(0, length)
+            this._complete = true
+          }
+          this._message += this._lastChunk
+          if (this._complete) {
+            return {
+              value: this._message,
+              done: true,
+            }
           }
         }
       }
