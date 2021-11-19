5f462dfb8de1f251b1efa0a05ef237e01642076a
heyderpd <heyderpd@gmail.com>
2021-11-19 00:45:02 -0300

    rewrite test

---
+++ test/01.spec.ts
@@ -0,0 +1,28 @@
+import assert from 'assert'
+import fs from 'fs'
+import { Load, Save } from '../src'
+
+const dataBefore = fs.readFileSync('./README.md', 'utf8')
+
+const testInjection = fileName => {
+  Save(`./test/before/${fileName}`, dataBefore)
+  return Load(`./test/before/${fileName}`)
+}
+
+describe('basic test', function() {
+  it('A.png', () => {
+    const dataAfter = testInjection('ia.png')
+    assert.deepEqual(
+      dataBefore,
+      dataAfter
+    )
+  })
+
+  it('B.png', () => {
+    const dataAfter = testInjection('ib.png')
+    assert.deepEqual(
+      dataBefore,
+      dataAfter
+    )
+  })
+})
