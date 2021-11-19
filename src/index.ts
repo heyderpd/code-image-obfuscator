5bdb4d2e4f857df24e60a2d450d53f3abc311274
heyderpd <heyderpd@gmail.com>
2021-11-19 00:37:34 -0300

    create new index of project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import Canvas from './canvas'
+import { SaveMessage, LoadMessage } from './data'
+
+
+export const Save = (imagePath: string, message: string) => {
+  const canvas = new Canvas(imagePath)
+  SaveMessage(canvas, message)
+}
+
+export const Load = (imagePath: string) => {
+  const canvas = new Canvas(imagePath)
+  LoadMessage(canvas)
+}
