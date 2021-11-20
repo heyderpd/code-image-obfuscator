4ec024c0ec030e90ae70767085c888b3e17a9a72
heyderpd <heyderpd@gmail.com>
2021-11-19 21:51:54 -0300

    better config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,10 @@
 
-
-        export const characterLimit: number = 3
-
 export const wordLength: number = 8
 
 export const pixelSize: number = 3
 
-export const pixelChunkSize: number = 8
+export const pixelChunkSize: number = 4
 
-export const messageChunkSize: number = 32 // 64 // 16
+export const messageChunkSize: number = 32
 
 export const messageVisible: boolean = false 
