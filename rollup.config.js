b337590225cbae2432e4ea413e8b073e10949a80
heyderpd <heyderpd@gmail.com>
2021-11-23 23:01:53 -0300

    create rollup config

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import { babel } from '@rollup/plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import { minify } from 'uglify-es'
+import uglify from 'rollup-plugin-uglify'
+
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'test/code-image-obfuscator.bundle.js',
+    format: 'iife',
+    name: 'cio',
+  },
+  plugins: [
+    replace({
+      'serverHandler': 'clientHandler',
+    }),
+    typescript(),
+    json(),
+    babel({
+      babelrc: false,
+      presets: [
+        [
+          "es2015", {
+            "modules": false
+          }
+        ]
+      ],
+      exclude: ['node_modules/**', 'src/canvas/server.ts'],
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+    }),
+    commonjs(),
+    uglify({}, minify),
+  ],
+}
