ba282bf57cd7c2e6728b2cbaa5d2020df6e9d4e8
heyderpd <heyderpd@gmail.com>
2021-11-26 19:51:11 -0300

    update tests

--- test/01.spec.ts
+++ test/01.spec.ts
@@ -1,32 +1,41 @@
 import * as assert from 'assert'
 import * as fs from 'fs'
-import { Load, Save } from '../src'
+import { SetMessageVisible, Load, Save } from '../src'
 
 const data = fs.readFileSync('./README.md', 'utf8')
 
-const testInjection = async fileName => {
+const testInjection = async (fileName, newFileName=null) => {
+  if (!newFileName) {
+    newFileName = fileName
+  } else {
+    SetMessageVisible(true)
+  }
   const originalFile = `./test/before/${fileName}`
-  const newFile = `./test/after/${fileName}`
+  const newFile = `./test/after/${newFileName}`
   await Save(originalFile, data, newFile)
   return await Load(newFile)
 }
 
 describe('basic test', function() {
 
-  it('A.png', async () => {
+  it('A.png', async function () {
     const dataAfter = await testInjection('ia.png')
-    assert.strictEqual(
-      data,
-      dataAfter
-    )
+    assert.strictEqual(data, dataAfter)
   })
 
-  it('B.png', async () => {
+  it('A-override.png', async function () {
+    const dataAfter = await testInjection('ia.png', 'ia-override.png')
+    assert.strictEqual(data, dataAfter)
+  })
+
+  it('B.png', async function () {
     const dataAfter = await testInjection('ib.png')
-    assert.strictEqual(
-      data,
-      dataAfter
-    )
+    assert.strictEqual(data, dataAfter)
   })
   
+  it('B.png', async function () {
+    const dataAfter = await testInjection('ib.png', 'ib-override.png')
+    assert.strictEqual(data, dataAfter)
+  })
+
 })
